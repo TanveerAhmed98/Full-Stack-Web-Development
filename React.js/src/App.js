88d1bdf718a8867d3d7e9b5286011826fc1a7b6e
tanveerahmed98 <tanveer.ahm98@gmail.com>
2021-01-16 21:06:01 +0500

    Project Codes

---
+++ React.js/src/App.js
@@ -0,0 +1,27 @@
+import React, {Component} from 'react';
+import { Navbar, NavbarBrand } from 'reactstrap';
+import Menu from './components/MenuComponents';
+import { DISHES } from './shared/dishes';
+
+class App extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      dishes: DISHES
+    };
+  }
+  render() {
+    return (
+      <div>
+        <Navbar dark color="success">
+          <div className="container">
+            <NavbarBrand href="/">Restaurant Confusion</NavbarBrand>
+          </div>
+        </Navbar>
+        <Menu dishes={this.state.dishes} />
+      </div>
+    );
+  }
+}
+
+export default App;
